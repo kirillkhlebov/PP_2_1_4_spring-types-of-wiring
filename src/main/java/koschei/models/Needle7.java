35844da0926519a2f9faadf117fb1198882a9993
Kirill Khlebov <kirill30003000@gmail.com>
2023-08-15 23:40:27 +0300

    Initial commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Death8 death;
+
+    public Needle7 (Death8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + death.toString();
     }
 }
